97c835922ce014ab2e4169162ede648ee5a4509d
Sun Woo Kim <53247724+sunwoo101@users.noreply.github.com>
2025-10-03 12:33:47 +1000

    Create longest_consecutive_sequence.py

---
+++ arrays_and_hasing/longest_consecutive_sequence.py
@@ -0,0 +1,37 @@
+from typing import List
+
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        checked_set = set()
+        nums_set = set()
+        max_count = 0
+        for value in nums:
+            nums_set.add(value)
+
+        for i in range(len(nums)):
+            if nums[i] - 1 not in nums_set and nums[i] not in checked_set:
+                checked_set.add(nums[i])
+                n = nums[i] + 1
+                count = 1
+                stop = False
+                while not stop:
+                    if n in nums_set:
+                        count += 1
+                        checked_set.add(n)
+                    else:
+                        stop = True
+                    n += 1
+
+                if count > max_count:
+                    max_count = count
+
+        return max_count
+
+
+solution = Solution()
+result = solution.longestConsecutive([2, 20, 4, 10, 3, 4, 5])
+print(result)
+
+result = solution.longestConsecutive([0, 3, 2, 5, 4, 6, 1, 1])
+print(result)
